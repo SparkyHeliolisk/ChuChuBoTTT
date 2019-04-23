a266febdc0f0491c1bdee21dcdac5c4c3752e107
SparkyHeliolisk <SparkyHeliolisk@users.noreply.github.com>
2019-04-23 11:13:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var nick = exports.nick = 'Waver-B'
 exports.pass = '12349';
 
 // A list of private rooms that the bot will not leak through the seen command
-exports.privaterooms = [];
+exports.privaterooms = ['groupchat-waverb-debatingsociety'];
 
 // The character text should start with to be seen as a command.
 // Note that using / and ! might be 'dangerous' since these are used in

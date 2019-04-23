18295837edaed8280a5e1c28771f1811fb3b2567
BaneOfAll <46022192+BaneOfAll@users.noreply.github.com>
2019-04-23 11:33:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.url = 'play.pokemonshowdown.com';
 // The nick and password to log in with
 var nick = exports.nick = 'Waver-B'
 exports.pass = '12349';
-
+exports.rooms = ['groupchat-waverb-debatingsociety', 'botdev'
 // A list of private rooms that the bot will not leak through the seen command
 exports.privaterooms = ['groupchat-waverb-debatingsociety'];
 
